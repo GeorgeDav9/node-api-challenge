1c49c858532fa6e3b5da70b3212a47c4657a51cd
George Davila <georgedavila@Georges-MacBook-Pro.local>
2020-04-10 12:08:20 -0400

    finished self study questions

---
+++ data/helpers/actionRouter.js
@@ -0,0 +1,6 @@
+// Express import
+const express = require('express');
+// Actions data import
+const Actions = require('./actionModel.js')
+// Router declaration
+const router = express.Router();
---
+++ data/helpers/projectRouter.js
@@ -0,0 +1,6 @@
+// Express import
+const express = require('express');
+// Project data import
+const Projects = require('./projectModel.js')
+// Router declaration
+const router = express.Router();
